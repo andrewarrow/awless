4edf66062c9d8ef89a7ba71d705ded2cf0382b1e
Andrew Arrow <andrew@bird.co>
2018-12-20 20:10:07 +0000

    start of s3

---
+++ commands/s3.go
@@ -0,0 +1,39 @@
+/*
+Copyright 2017 WALLIX
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package commands
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(s3Cmd)
+}
+
+var s3Cmd = &cobra.Command{
+	Use:               "s3",
+	Aliases:           []string{"s3"},
+	PersistentPreRun:  applyHooks(initAwlessEnvHook, initLoggerHook, initCloudServicesHook, firstInstallDoneHook),
+	PersistentPostRun: applyHooks(verifyNewVersionHook, onVersionUpgrade, networkMonitorHook),
+	Short:             "s3",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("hi")
+	},
+}
